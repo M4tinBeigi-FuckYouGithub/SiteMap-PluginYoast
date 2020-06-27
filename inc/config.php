520e3f5f8a81fb766adb7830a7341351ad2d296d
Matin Beigi <45142752+matinbeigi99@users.noreply.github.com>
2020-06-27 05:40:35 -0700

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,11 @@
+<?PHP
+defined("blogname") or define("blogname","SiteMap");
+
+defined("localhost") or define("localhost","");
+defined("database") or define("database","");
+defined("username") or define("username","");
+defined("password") or define("password","");
+defined("url") or define("url","https://esgtrade.com/");
+defined("urlsitemap") or define("urlsitemap","//esgtrade.com/sitemap/");
+defined("class") or define("class","inc/class/");
+
